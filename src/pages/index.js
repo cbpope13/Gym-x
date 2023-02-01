05a7db7db98d06d8cfae190e021b7ff3ac483296
Cade Pope <cbpope13@gmail.com>
2023-01-31 22:01:29 -0500

    updated favicon and added animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Home() {
 	return (
@@ -9,17 +10,28 @@ export default function Home() {
 			<main>
 				<div id="home" className="bg-red-600 flex items-center justify-between">
 					<Image src="/gym2-removebg.png" alt="Hero" width="600" height="500" />
-					<div className="text-white mr-32 flex flex-col items-center">
-						<h1 className="text-[80px]">
+					<motion.div
+						initial={{ y: '100px', opacity: '0%' }}
+						animate={{
+							y: '0px',
+							opacity: '100%',
+							transition: { duration: 1, delay: 0.25 },
+						}}
+						className="text-white mr-32 flex flex-col items-center"
+					>
+						<p className="text-[80px]">
 							<i>your world class</i>
-						</h1>
-						<p className="text-[30px] font-sans font-light -mt-4">
+						</p>
+						<p
+							animate={{ fontSize: '30px' }}
+							className=" text-[30px] font-sans font-light -mt-4"
+						>
 							FITNESS CENTER AND HEALTH CLUB
 						</p>
 						<button className="bg-neutral-900 w-48 py-3 mt-6 rounded-sm text-xl text-red-600 hover:shadow-2xl">
 							<i>Sign up today!</i>
 						</button>
-					</div>
+					</motion.div>
 				</div>
 				{/*What we do section*/}
 				<div className="bg-neutral-800 flex p-6 relative">
@@ -28,10 +40,28 @@ export default function Home() {
 					</div>
 					<div className="">
 						<div className="bg-neutral-900 bg-opacity-50 text-white p-4 max-w-xl flex flex-col space-y-6 absolute right-6">
-							<p className="text-6xl">
+							<motion.p
+								initial={{ y: '100px', opacity: '0%' }}
+								whileInView={{
+									y: '0px',
+									opacity: '100%',
+									transition: { duration: 0.5, delay: 0.25 },
+								}}
+								viewport={{ once: true }}
+								className="text-6xl"
+							>
 								<i> What we do</i>
-							</p>
-							<p className="font-sans">
+							</motion.p>
+							<motion.p
+								initial={{ y: '100px', opacity: '0%' }}
+								whileInView={{
+									y: '0px',
+									opacity: '100%',
+									transition: { duration: 0.5, delay: 0.25 },
+								}}
+								viewport={{ once: true }}
+								className="font-sans"
+							>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
 								atque iusto nihil culpa. Id perferendis at eligendi deserunt
 								molestiae facere est illum facilis aut sunt repellat harum dolor
@@ -40,13 +70,22 @@ export default function Home() {
 								temporibus doloremque? Quae officia esse voluptatibus vero et
 								itaque. Ratione, rerum! Dolore tempora tempore fugit libero
 								totam iusto?
-							</p>
+							</motion.p>
 						</div>
 					</div>
 				</div>
 				{/*CTA*/}
 				<div className="bg-red-600 flex justify-between items-center px-48 py-8">
-					<button className="bg-neutral-900 rounded-sm text-red-600 text-xl flex space-x-3 items-center px-8 py-2 hover:shadow-2xl">
+					<motion.button
+						initial={{ x: '-200px', opacity: '0%' }}
+						whileInView={{
+							x: '0px',
+							opacity: '100%',
+							transition: { duration: 0.5, delay: 0.25 },
+						}}
+						viewport={{ once: true }}
+						className="bg-neutral-900 rounded-sm text-red-600 text-xl flex space-x-3 items-center px-8 py-2 hover:shadow-2xl"
+					>
 						<i>Start today</i>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -62,8 +101,17 @@ export default function Home() {
 								d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
 							/>
 						</svg>
-					</button>
-					<div className="flex flex-col items-center">
+					</motion.button>
+					<motion.div
+						initial={{ x: '200px', opacity: '0%' }}
+						whileInView={{
+							x: '0px',
+							opacity: '100%',
+							transition: { duration: 0.5, delay: 0.25 },
+						}}
+						viewport={{ once: true }}
+						className="flex flex-col items-center"
+					>
 						<i className="text-3xl tracking-widest">
 							THE BEST GYM MEMBERSHIPS AROUND
 						</i>
@@ -71,11 +119,20 @@ export default function Home() {
 							There’s a reason people consider us the best gym in the area—try
 							us out today!
 						</p>
-					</div>
+					</motion.div>
 				</div>
 				{/*About us*/}
 				<div className="bg-neutral-900 text-white text-center p-28 flex flex-col space-y-10">
-					<div className="flex flex-col space-y-3">
+					<motion.div
+						initial={{ y: '100px', opacity: '0%' }}
+						whileInView={{
+							y: '0px',
+							opacity: '100%',
+							transition: { duration: 0.5, delay: 0.25 },
+						}}
+						viewport={{ once: true }}
+						className="flex flex-col space-y-3"
+					>
 						<i className="text-6xl tracking-wide">Welcome to gym-x</i>
 						<p className="font-sans font-extralight text-xl">
 							YOUR WORLD CLASS FITNESS CENTER AND HEALTH CLUB
@@ -88,9 +145,17 @@ export default function Home() {
 							challenging, and rewarding. We are more than a fitness center;
 							we’re a health club in the truest sense of the word.
 						</p>
-					</div>
+					</motion.div>
 					<div className="flex justify-center">
-						<button className="bg-red-600 rounded-sm text-white text-xl flex space-x-3 items-center px-6 py-1 hover:bg-red-700 transition duration-200">
+						<motion.button
+							initial={{ y: '100px', opacity: '0%' }}
+							whileInView={{
+								y: '0px',
+								opacity: '100%',
+							}}
+							viewport={{ once: true }}
+							className="bg-red-600 rounded-sm text-white text-xl flex space-x-3 items-center px-6 py-1 hover:bg-red-700 transition duration-200"
+						>
 							<i>Get Started</i>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -106,7 +171,7 @@ export default function Home() {
 									d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
 								/>
 							</svg>
-						</button>
+						</motion.button>
 					</div>
 				</div>
 				{/*Memberships*/}
@@ -116,7 +181,16 @@ export default function Home() {
 				>
 					<i className="text-6xl">Memberships</i>
 					<div className="flex flex-col space-y-6 w-full">
-						<div className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full">
+						<motion.div
+							initial={{ x: '-500px', opacity: '0%' }}
+							whileInView={{
+								x: '0px',
+								opacity: '100%',
+								transition: { duration: 1, delay: 0.25 },
+							}}
+							viewport={{ once: true }}
+							className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full"
+						>
 							<div>
 								<p className="text-3xl tracking-wide">
 									0 Down 29.99 Per Month 12 Month Membership
@@ -128,8 +202,17 @@ export default function Home() {
 							<button className="bg-red-600 rounded-sm text-white text-xl px-6 py-1 hover:bg-red-700 transition duration-200">
 								<i>Join Now</i>
 							</button>
-						</div>
-						<div className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full">
+						</motion.div>
+						<motion.div
+							initial={{ x: '500px', opacity: '0%' }}
+							whileInView={{
+								x: '0px',
+								opacity: '100%',
+								transition: { duration: 1, delay: 0.25 },
+							}}
+							viewport={{ once: true }}
+							className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full"
+						>
 							<div>
 								<p className="text-3xl tracking-wide">
 									19.99 Per Month 12 Month Membership
@@ -142,8 +225,17 @@ export default function Home() {
 							<button className="bg-red-600 rounded-sm text-white text-xl px-6 py-1 hover:bg-red-700 transition duration-200">
 								<i>Join Now</i>
 							</button>
-						</div>
-						<div className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full">
+						</motion.div>
+						<motion.div
+							initial={{ x: '-500px', opacity: '0%' }}
+							whileInView={{
+								x: '0px',
+								opacity: '100%',
+								transition: { duration: 1, delay: 0.25 },
+							}}
+							viewport={{ once: true }}
+							className="border-dashed border-black border px-6 py-4 flex items-center justify-between rounded-xl w-full"
+						>
 							<div>
 								<p className="text-3xl tracking-wide">
 									24.99 Month to Month Membership
@@ -157,7 +249,7 @@ export default function Home() {
 							<button className="bg-red-600 rounded-sm text-white text-xl px-6 py-1 hover:bg-red-700 transition duration-200">
 								<i>Join Now</i>
 							</button>
-						</div>
+						</motion.div>
 					</div>
 				</div>
 				{/*Personal training*/}
@@ -169,10 +261,28 @@ export default function Home() {
 						className="-translate-x-48"
 					/>
 					<div className="absolute top-10 right-24 flex flex-col items-center space-y-6">
-						<i className="text-white text-6xl text-center">
+						<motion.i
+							initial={{ y: '100px', opacity: '0%' }}
+							whileInView={{
+								y: '0px',
+								opacity: '100%',
+								transition: { duration: 0.5, delay: 0.25 },
+							}}
+							viewport={{ once: true }}
+							className="text-white text-6xl text-center"
+						>
 							ONE-ON-ONE <br /> PERSONAL <br /> TRAINING
-						</i>
-						<div className="bg-neutral-900 p-6 flex flex-col space-y-6">
+						</motion.i>
+						<motion.div
+							initial={{ y: '100px', opacity: '0%' }}
+							whileInView={{
+								y: '0px',
+								opacity: '100%',
+								transition: { duration: 0.5, delay: 0.25 },
+							}}
+							viewport={{ once: true }}
+							className="bg-neutral-900 p-6 flex flex-col space-y-6"
+						>
 							<p className="text-red-600 text-2xl text-center font-sans font-extralight">
 								Try us out
 							</p>
@@ -224,7 +334,7 @@ export default function Home() {
 									<i>Get Started today!</i>
 								</button>
 							</div>
-						</div>
+						</motion.div>
 					</div>
 				</div>
 			</main>
